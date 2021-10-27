a007cc4193f872a7134debbad2d01f05ac4bee4a
Gabriel <gonzagadinizgabrieltadeu@gmail.com>
2021-10-27 14:10:20 -0300

    adicionando funcionalidades ao projeto de javascript baseado no curso da alura

---
+++ projeto-js-alura/componentes/deletaTarefa.js
@@ -0,0 +1,17 @@
+const BotaoDeleta = () => {
+  const botaoDeleta = document.createElement('button');
+
+  botaoDeleta.classList.add('check-button');
+  botaoDeleta.innerText = 'deletar';
+  botaoDeleta.addEventListener('click', deletarTarefa);
+  return botaoDeleta;
+}
+
+const deletarTarefa = (evento) => {
+  const botaoDeleta = evento.target;
+
+  const tarefaDeletada = botaoDeleta.parentElement;
+  tarefaDeletada.remove();
+}
+
+export default BotaoDeleta;
